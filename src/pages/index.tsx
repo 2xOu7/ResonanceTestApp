2a887522bdf15693bc3882d99cbb37929fb32e8c
2xOu7 <jonathanou1998@gmail.com>
2024-02-22 23:56:31 -0800

    add resonance client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import {Button} from "@mui/material";
+import {Resonance} from "resonance-client";
 
 export default function Home() {
   return (
     <div style={{textAlign: 'center', marginTop: '5vh'}}>
+        <Resonance apiKey={'603971ba-d787-45c0-8d88-a10053bf1616'} eventContext={{}} externalUserId={'123'}/>
         <Button variant={'outlined'} style={{backgroundColor: 'deeppink', color: 'white', marginRight: '1vw'}}>Pink Button</Button>
         <Button variant={'outlined'} style={{backgroundColor: 'blue', color: 'white', marginRight: '1vw'}}>Blue Button</Button>
         <Button variant={'outlined'} style={{backgroundColor: 'green', color: 'white', marginRight: '1vw'}}>Green Button</Button>
