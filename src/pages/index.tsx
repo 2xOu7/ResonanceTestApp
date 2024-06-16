661371bd78274cbcb987cfc198cf8b6f9cb096a8
2xOu7 <jonathanou1998@gmail.com>
2024-06-16 02:22:03 -0700

    add chameleon test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,100 +1,68 @@
 import { Button } from '@mui/material'
-import {
-  notifyEvent,
-  MicrocopyContext,
-  PromptCampaignContext,
-} from 'resonance-client'
-import NonHeadlessSimpleModal from '../components/NonHeadlessSimpleModal'
-import Tooltip from '../components/Tooltip'
-import React, { Component } from 'react'
-
-class EmbeddedMicrocopyExperiment extends Component<{}, {}> {
-  componentDidMount() {}
-
-  render() {
-    return (
-      <PromptCampaignContext.Consumer>
-        {({ campaignToRender }) => {
-          if (
-            campaignToRender === null ||
-            campaignToRender.campaignId !==
-              'cd0a2aa2-c1ce-4515-8d04-5bf65b5379d0'
-          ) {
-            return null
-          }
-
-          return <p>{campaignToRender.variantResult.content['text']}</p>
-        }}
-      </PromptCampaignContext.Consumer>
-    )
-  }
-}
+import { notifyEvent } from 'resonance-client'
+import React from 'react'
 
 export default function Home() {
+  const chameleon = require('@chamaeleonidae/chmln')
+  chameleon.init(
+    'S84xJ6DS6NJV1NT6wpgtCJ58XwnSXcoZC9e2IjTaKqCWDl-1ShZoy-FdUvTuEU0fJ4Dtyj',
+    { fastUrl: 'https://fast.chameleon.io/' }
+  )
+
+  chameleon.identify('123')
+
   return (
-    <MicrocopyContext.Consumer>
-      {({ copies }) => {
-        return (
-          <div style={{ textAlign: 'center', marginTop: '5vh' }}>
-            <Button
-              variant={'outlined'}
-              style={{
-                backgroundColor: 'deeppink',
-                color: 'white',
-                marginRight: '1vw',
-              }}
-              onClick={() => {
-                notifyEvent('pink_button_clicked')
-              }}
-            >
-              Pink Button
-            </Button>
-            <Button
-              data-tooltip-id={'blue_button_tooltip'}
-              variant={'outlined'}
-              style={{
-                backgroundColor: 'blue',
-                color: 'white',
-                marginRight: '1vw',
-              }}
-              onClick={() => {
-                notifyEvent('blue_button_clicked')
-              }}
-            >
-              Blue Button
-            </Button>
-            <Button
-              variant={'outlined'}
-              style={{
-                backgroundColor: 'green',
-                color: 'white',
-                marginRight: '1vw',
-              }}
-              onClick={() => {
-                notifyEvent('green_button_clicked')
-              }}
-            >
-              Green Button
-            </Button>
-            <Button
-              data-tooltip-id={'red_button_tooltip'}
-              variant={'outlined'}
-              style={{ backgroundColor: 'red', color: 'white' }}
-              onClick={() => {
-                notifyEvent('red_button_clicked')
-              }}
-            >
-              {copies.filter((c) => c.selector === 'red_button').length === 0
-                ? 'Red Button'
-                : copies.filter((c) => c.selector === 'red_button')[0].variant
-                    .copy}
-            </Button>
-            <NonHeadlessSimpleModal />
-            <Tooltip />
-            <EmbeddedMicrocopyExperiment />
-          </div>
-        )
-      }}
-    </MicrocopyContext.Consumer>
+    <div style={{ textAlign: 'center', marginTop: '5vh' }}>
+      <Button
+        variant={'outlined'}
+        style={{
+          backgroundColor: 'deeppink',
+          color: 'white',
+          marginRight: '1vw',
+        }}
+        onClick={() => {
+          notifyEvent('pink_button_clicked')
+        }}
+      >
+        Pink Button
+      </Button>
+      <Button
+        data-tooltip-id={'blue_button_tooltip'}
+        variant={'outlined'}
+        style={{
+          backgroundColor: 'blue',
+          color: 'white',
+          marginRight: '1vw',
+        }}
+        onClick={() => {
+          notifyEvent('blue_button_clicked')
+        }}
+      >
+        Blue Button
+      </Button>
+      <Button
+        variant={'outlined'}
+        style={{
+          backgroundColor: 'green',
+          color: 'white',
+          marginRight: '1vw',
+        }}
+        onClick={() => {
+          notifyEvent('green_button_clicked')
+        }}
+      >
+        Green Button
+      </Button>
+      <Button
+        data-tooltip-id={'red_button_tooltip'}
+        variant={'outlined'}
+        style={{ backgroundColor: 'red', color: 'white' }}
+        onClick={() => {
+          notifyEvent('red_button_clicked')
+        }}
+      >
+        Red Button
+      </Button>
+    </div>
   )
 }
