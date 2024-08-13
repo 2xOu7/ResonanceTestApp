65d806f62478c12ee399dbdc3cbda82cb8b6dd3c
2xOu7 <jonathanou1998@gmail.com>
2024-08-13 00:31:05 -0700

    update api key

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,12 +10,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ResonanceMicrocopyProvider
       externalUserId={'124'}
-      apiKey={'603971ba-d787-45c0-8d88-a10053bf1616'}
+      apiKey={'39eaf65b62a49a1e5efba6c32a14b083b8adf79d63ec417fcbcb41a10704116e0dbf6b1163c9a69981349f91ff23b890'}
       eventContext={{}}
     >
       <Resonance
         externalUserId={'124'}
-        apiKey={'603971ba-d787-45c0-8d88-a10053bf1616'}
+        apiKey={'39eaf65b62a49a1e5efba6c32a14b083b8adf79d63ec417fcbcb41a10704116e0dbf6b1163c9a69981349f91ff23b890'}
         eventContext={{
           "User First Name": "Jane",
           "User Last Name": "Doe"
