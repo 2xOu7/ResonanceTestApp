5de92430aab5dba1a63c160ec2f3c21c313768c9
2xOu7 <jonathanou1998@gmail.com>
2024-09-15 16:26:46 -0700

    use the role

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,15 @@ import '@fontsource/roboto/700.css'
 import type { AppProps } from 'next/app'
 import 'react-tooltip/dist/react-tooltip.css'
 import { Resonance } from 'resonance-client'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [role, setRole] = useState<string>('')
+
   return (
+      <>
+        <p>Role</p>
+        <input onChange={e => setRole(e.target.value)} value={role} />
       <Resonance
         externalUserId={'125'}
         apiKey={'c04b043b200f6cf9d191fa8826550d62e3e4abe37c6eb86b13aaefa413d0b234dd2742713142812aa0c25188ac587be2'}
@@ -15,9 +21,10 @@ export default function App({ Component, pageProps }: AppProps) {
           "User First Name": "Jane",
           "User Last Name": "Doe"
         }}
-        userAttributes={{}}
+        userAttributes={{role}}
       >
         <Component {...pageProps} />
       </Resonance>
+      </>
   )
 }
