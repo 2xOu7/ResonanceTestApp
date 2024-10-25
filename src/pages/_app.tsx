aecbfeacb7a668963cc3e72be8a358234909c5e6
Jonathan Ou <jonathanou@Jonathans-MacBook-Pro.local>
2024-10-24 18:43:16 -0700

    add back api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
       <p>User ID</p>
       <input onChange={e => setUserId(e.target.value)} value={userId} />
-      <ResonanceMicrocopyProvider externalUserId={userId} apiKey={'c04b043b200f6cf9d191fa8826550d62e3e4abe37c6eb86b13aaefa413d0b234dd2742713142812aa0c25188ac587be2'} eventContext={{
+      <ResonanceMicrocopyProvider apiUrl={'https://app.useresonance.com'} externalUserId={userId} apiKey={'c04b043b200f6cf9d191fa8826550d62e3e4abe37c6eb86b13aaefa413d0b234dd2742713142812aa0c25188ac587be2'} eventContext={{
         "User First Name": "Jane",
         "User Last Name": "Doe"
       }} userAttributes={{ role }}>
