80e92e6f70271e1251a9694bcad41e996d7c6932
Jonathan Ou <jonathanou@Jonathans-MacBook-Pro.local>
2025-01-12 17:06:35 -0800

    sidehseet

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -24,6 +24,7 @@ export default class SimpleTooltip extends Component<{}, {}> {
 
           return (
             <Tooltip
+              style={{ backgroundColor: 'white', color: 'black' }}
               id={content['selector']}
               content={content['header']}
               isOpen={true}
