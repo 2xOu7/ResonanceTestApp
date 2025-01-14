6fddafb66f73ac280060dd71b8b62afaf773cf7e
Jonathan Ou <jonathanou@Jonathans-MacBook-Pro.local>
2025-01-14 00:58:50 -0800

    almost fking done

--- src/pages/chameleon.tsx
+++ src/pages/chameleon.tsx
@@ -1,7 +1,21 @@
 import { Component } from 'react'
 import { Button } from 'evergreen-ui'
+import axios from 'axios'
 
-export default class Chameleon extends Component<{}, {}> {
+const axiosClient = axios.create()
+export default class Chameleon extends Component<
+  {},
+  {
+    idToShow: string
+  }
+> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      idToShow: 'placeholder',
+    }
+  }
+
   componentDidMount() {
     const chameleon = require('@chamaeleonidae/chmln')
     chameleon.init(
@@ -18,10 +32,22 @@ export default class Chameleon extends Component<{}, {}> {
   render() {
     return (
       <div style={{ marginTop: '5vh', textAlign: 'center' }}>
-        <div id={'placeholder'} />
+        <div id={this.state.idToShow} />
         <Button
           onClick={async () => {
-            alert('Hello')
+            const response = await axiosClient.post(
+              'https://app.staging.useresonance.com/api/chameleon/getbestmessages',
+              {},
+              {
+                headers: {
+                  Authorization:
+                    'Bearer a73143d411c6ce081479fbf6136659ad75f5ee6e459476f8a26f2090908fc9d52fe89e8f1b283cb253f687e77aebc5a2',
+                },
+              }
+            )
+
+            const castedData = response.data as { id: string }
+            this.setState({ ...this.state, idToShow: castedData.id })
           }}
         >
           Chameleon!
