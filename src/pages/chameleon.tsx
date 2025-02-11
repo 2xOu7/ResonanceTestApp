b3f829f490170f1702fc7b1b548a886bd68137d9
kp248163264 <kp248163264@gmail.com>
2025-02-10 21:08:35 -0800

    back

--- src/pages/chameleon.tsx
+++ src/pages/chameleon.tsx
@@ -1,38 +1,103 @@
-import React, { Component } from 'react';
-import { Heading, Pane } from 'evergreen-ui';
-import axios from 'axios';
+import React, { Component } from 'react'
+import { Button, Heading, Pane } from 'evergreen-ui'
+import axios from 'axios'
 
-export default class Appcues extends Component {
-  componentDidMount() {
-    if (window.Appcues) {
-      axios.post(
-        'http://localhost:3000/api/appcues/getbestmessages',
-        {},
-        {
-          headers: {
-            Authorization: `Bearer 8928257a2acdda374fb314d0a845f73a3b3d9118a77c6c0f17d5bae97e29283e54a0e7fc097665de42fc7efb1ef483ad`,
-          },
-        }
-      ).then(({ data }) => {
-        window.Appcues.identify('test_user_id', {
-          name: 'Katherine Pioro',
-          email: 'katherine@useresonance.com',
-          test: data
-        });
-
-        window.Appcues.show("05888a59-8aec-4b55-9222-0b97156851a7");
-      }).catch(error => console.error("Error fetching Appcues data:", error));
+const axiosClient = axios.create()
+export default class Chameleon extends Component<
+  {},
+  {
+    idToShow: string
+  }
+> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      idToShow: 'placeholder',
     }
+  }
+
+  async componentDidMount() {
+    const { data } = await axios.post(
+      'https://app.staging.useresonance.com/api/chameleon/getbestmessages',
+      {
+        externalUserId: 'jonathan',
+      },
+      {
+        headers: {
+          Authorization: `Bearer a73143d411c6ce081479fbf6136659ad75f5ee6e459476f8a26f2090908fc9d52fe89e8f1b283cb253f687e77aebc5a2`,
+        },
+      }
+    )
+
+    const castedData = data as {
+      [key: string]: {
+        content: { [key: string]: string }
+        variantId: string
+      }
+    }
+
+    window.getField = (
+      campaignId: string,
+      fieldName: string,
+      fallback: string
+    ): string => {
+      const val = castedData?.[campaignId]?.content?.[fieldName]
+      return val ? val : fallback
+    }
+
+    window.logImpression = (campaignId: string, variantId: string) => {
+      console.log(campaignId)
+      console.log(variantId)
+    }
+
+    const chameleon = require('@chamaeleonidae/chmln')
+    chameleon.init(
+      'ShKgqpN5FMwCGZIfkx0SPgxWna8Zsn6pVXdPuLrTEADVlN-1TxxhH-FFoTg0sbJ9YYycdj',
+      { fastUrl: 'https://fast.chameleon.io/' }
+    )
+
+    chameleon.identify('123', {
+      email: 'katherine@yourresonate.com',
+      name: 'Katya Pioro',
+    })
   }
 
   render() {
     return (
-      <>
-        <Pane display="flex" padding={16} background="tint2" borderRadius={3} style={{ marginTop: -50 }}>
-          <Heading size={1000} style={{ marginLeft: 250 }}>AppCues</Heading>
+      <div style={{ marginTop: '5vh', textAlign: 'center' }}>
+        <div id={this.state.idToShow} />
+        <Pane
+          display={'flex'}
+          padding={16}
+          background={'tint2'}
+          borderRadius={3}
+          style={{ marginTop: -50 }}
+        >
+          <Heading size={1000} style={{ marginLeft: 250 }}>
+            Chameleon
+          </Heading>
         </Pane>
-        <div style={{ marginTop: '5vh', textAlign: 'center' }}></div>
-      </>
-    );
+        <Button
+          onClick={async () => {
+            const response = await axiosClient.post(
+              'https://app.staging.useresonance.com/api/chameleon/getbestmessages',
+              {},
+              {
+                headers: {
+                  Authorization:
+                    'Bearer a73143d411c6ce081479fbf6136659ad75f5ee6e459476f8a26f2090908fc9d52fe89e8f1b283cb253f687e77aebc5a2',
+                },
+              }
+            )
+
+            const castedData = response.data as { id: string }
+            this.setState({ ...this.state, idToShow: castedData.id })
+          }}
+          style={{ marginTop: 25 }}
+        >
+          Chameleon!
+        </Button>
+      </div>
+    )
   }
 }
