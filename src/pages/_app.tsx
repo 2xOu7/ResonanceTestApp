504653e36702e61bfe77c9d29216db8117433fb3
Jonathan Ou <jonathanou@Jonathans-MacBook-Pro.local>
2025-03-06 18:33:21 -0800

    done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,15 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import type { AppProps } from 'next/app'
 import 'react-tooltip/dist/react-tooltip.css'
-import {
-  ResonanceConversionLogger,
-  ResonanceCrossChannelClient,
-  ResonanceMicrocopyProvider,
-} from 'resonance-client'
 import React, { useEffect, useState } from 'react'
 import Sidebar from '@/components/Sidebar'
 import { Box } from '@mui/material'
-import { Intercom, update } from '@intercom/messenger-js-sdk'
+import { Intercom } from '@intercom/messenger-js-sdk'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [role, setRole] = useState<string>('owner')
   const [userId, setUserId] = useState<string>(
-    'db97b059-cbe8-454b-9ab1-7ee38499222e',
+    'db97b059-cbe8-454b-9ab1-7ee38499222e'
   )
 
   useEffect(() => {
@@ -29,7 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
         email: 'katherine@useresonance.com',
         created_at: 123,
         custom_attribute: 'test',
-      });
+      })
     }
   }, [])
 
@@ -44,44 +39,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Sidebar />
-      <Box sx={{ marginLeft: 35, marginTop: 10 }}>
-
-      </Box>
-      <ResonanceConversionLogger
-        apiUrl={'https://app.staging.useresonance.com'}
-        externalUserId={userId}
-        apiKey={
-          '3b2a055a03b91b08fe1af786ece89a9046ed5f64cecda06f533dadd1907d8e20b4d4e4dc7632719213dd71bd80d5074d'
-        }
-        userAttributes={{ role }}
-      />
-      <ResonanceMicrocopyProvider
-        apiUrl={'https://app.staging.useresonance.com'}
-        externalUserId={userId}
-        apiKey={
-          '3b2a055a03b91b08fe1af786ece89a9046ed5f64cecda06f533dadd1907d8e20b4d4e4dc7632719213dd71bd80d5074d'
-        }
-        eventContext={{
-          'User First Name': 'Jane',
-          'User Last Name': 'Doe',
-        }}
-        userAttributes={{ role }}
-      >
-        <ResonanceCrossChannelClient
-          externalUserId={userId}
-          apiKey={
-            '3b2a055a03b91b08fe1af786ece89a9046ed5f64cecda06f533dadd1907d8e20b4d4e4dc7632719213dd71bd80d5074d'
-          }
-          eventContext={{
-            'User First Name': 'Jane',
-            'User Last Name': 'Doe',
-          }}
-          userAttributes={{ role }}
-          apiUrl={'https://app.staging.useresonance.com'}
-        >
-          <Component {...customPageProps} />
-        </ResonanceCrossChannelClient>
-      </ResonanceMicrocopyProvider>
+      <Box sx={{ marginLeft: 35, marginTop: 10 }}></Box>
+      <Component {...customPageProps} />
     </>
   )
 }
