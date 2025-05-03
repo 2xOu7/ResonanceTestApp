efb55385b16a4fe08502358b350f5e8912969752
Jonathan Ou <jonathanou@Jonathans-MacBook-Pro.local>
2025-05-03 02:02:41 -0700

    deploy into prod

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,10 +1,36 @@
-self.onfetch = (event) => {
+self.addEventListener('install', (event) => {
+  self.skipWaiting() // Activate worker immediately
+})
+
+self.addEventListener('activate', (event) => {
+  self.clients.claim() // Take control of all pages immediately
+})
+
+self.onfetch = async (event) => {
   event.respondWith(
     (async () => {
-      console.log(event)
+      const reqClone = event.request.clone()
       const response = await fetch(event.request)
-      console.log(response)
-      return response
+      try {
+        const body = await reqClone.json()
+        const resClone = response.clone()
+        const resJson = await resClone.json()
+
+        console.log({
+          request: { url: reqClone.url, body },
+          response: { body: resJson, url: resClone.url },
+        })
+
+        return response
+      } catch (e) {
+      } finally {
+        return response
+      }
     })()
   )
+}
+
+sleep = async (ms) => {
+  console.log('Sleeping')
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
